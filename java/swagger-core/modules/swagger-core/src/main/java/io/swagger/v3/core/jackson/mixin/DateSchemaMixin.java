199ffae8e1a4def7523bebac8b0c012e81f0dbbc
1shekhar <42611113+1shekhar@users.noreply.github.com>
2020-07-15 18:05:28 +0530

    Revert "Add java code to master"

--- java/swagger-core/modules/swagger-core/src/main/java/io/swagger/v3/core/jackson/mixin/DateSchemaMixin.java
+++
@@ -1,9 +0,0 @@
-package io.swagger.v3.core.jackson.mixin;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-
-public abstract class DateSchemaMixin {
-
-    @JsonFormat (shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    public abstract Object getExample();
-}
--- java/swagger-core/modules/swagger-integration/src/main/java/io/swagger/v3/oas/integration/OpenApiConfigurationException.java
+++
@@ -1,8 +0,0 @@
-package io.swagger.v3.oas.integration;
-
-public class OpenApiConfigurationException extends Exception {
-
-    public OpenApiConfigurationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
